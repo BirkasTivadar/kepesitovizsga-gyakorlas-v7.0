55569c8e1bc2d55b1e97f7daa68a00a416ec8577
BirkasTivadar <birkastivadar@gmail.com>
2021-05-06 08:43:47 +0200

    FineLongWordOnceAgain kész

--- src/main/java/hu/nive/ujratervezes/kepesitovizsga/finelongwordonceagain/FineLongWordOnceAgain.java
+++ src/main/java/hu/nive/ujratervezes/kepesitovizsga/finelongwordonceagain/FineLongWordOnceAgain.java
@@ -2,4 +2,23 @@ package hu.nive.ujratervezes.kepesitovizsga.finelongwordonceagain;
 
 public class FineLongWordOnceAgain {
 
+
+    public char[][] getFineLongWordOnceAgainButNowInAReverseWay(String word, int lengthOfParts) {
+
+        if (lengthOfParts > word.length()) {
+            throw new IllegalArgumentException("Number of letters cannot be more than length of the word!");
+        }
+
+        char[][] result = new char[word.length() - lengthOfParts + 1][];
+        int rowCounter = 0;
+
+        while (word.length() >= lengthOfParts) {
+            String part = word.substring(0, lengthOfParts).toUpperCase();
+            word = word.substring(1);
+            result[rowCounter] = part.toCharArray();
+            rowCounter++;
+        }
+
+        return result;
+    }
 }
--- src/test/java/hu/nive/ujratervezes/kepesitovizsga/finelongwordonceagain/FineLongWordOnceAgainTest.java
+++ src/test/java/hu/nive/ujratervezes/kepesitovizsga/finelongwordonceagain/FineLongWordOnceAgainTest.java
@@ -1,23 +1,25 @@
 package hu.nive.ujratervezes.kepesitovizsga.finelongwordonceagain;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FineLongWordOnceAgainTest {
-/*
+
     @Test
     public void testGetFineLongWordOnceAgainButNowInAReverseWay() {
         char[][] fineLongWordInAnArrayOfArrays = new FineLongWordOnceAgain().getFineLongWordOnceAgainButNowInAReverseWay("IllegalArgumentException", 6);
 
-        Assertions.assertEquals(19, fineLongWordInAnArrayOfArrays.length);
-        Assertions.assertEquals('G', fineLongWordInAnArrayOfArrays[2][2]);
-        Assertions.assertEquals('X', fineLongWordInAnArrayOfArrays[12][4]);
+        assertEquals(19, fineLongWordInAnArrayOfArrays.length);
+        assertEquals('G', fineLongWordInAnArrayOfArrays[2][2]);
+        assertEquals('X', fineLongWordInAnArrayOfArrays[12][4]);
     }
 
     @Test
     public void testGetFineLongWordOnceAgainButNowInAReverseWayWithIllegalParameter() {
-        Exception ex = Assertions.assertThrows(IllegalArgumentException.class,
+        Exception ex = assertThrows(IllegalArgumentException.class,
                 () -> new FineLongWordOnceAgain().getFineLongWordOnceAgainButNowInAReverseWay("IllegalArgumentException", 100));
-        Assertions.assertEquals("Number of letters cannot be more than length of the word!", ex.getMessage());
+        assertEquals("Number of letters cannot be more than length of the word!", ex.getMessage());
     }
-*/
+
 }
